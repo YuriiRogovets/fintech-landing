7eac15410fb4f1867ec60fe566a5370861774ecf
YuriiRogovets <yura.rogovets@gmail.com>
2025-06-25 19:13:25 +0300

    create footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import styles from "./Footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={styles.footer}>
+      <div className={styles.topSection}>
+        <div className={styles.logoBlock}>
+          <Image src="/icons/kvitka-lolo.svg" alt="Kvitka" width={120} height={40} />
+          <ul className={styles.socials}>
+            <li>
+              <a href="#" aria-label="instagram">
+                <Image src="/icons/instagram.svg" alt="instagram icon" width={12} height={12} />
+              </a>
+            </li>
+            <li>
+              <a href="#" aria-label="facebook">
+                <Image src="/icons/facebook.svg" alt="facebook icon" width={12} height={12} />
+              </a>
+            </li>
+            <li>
+              <a href="#" aria-label="linkedIn">
+                <Image src="/icons/linkedin.svg" alt="In icon" width={12} height={12} />
+              </a>
+            </li>
+            
+            <li>
+              <a href="#" aria-label="tik_tok">
+                <Image src="/icons/tik_tok.svg" alt="tik_tok icon" width={11} height={12} />
+              </a>
+            </li>
+            
+            <li>
+              <a href="#" aria-label="youtube">
+                <Image src="/icons/youtube.svg" alt="youtube icon" width={12} height={8} />
+              </a>
+            </li>
+            <li>
+              <a href="#" aria-label="telegram">
+                <Image src="/icons/telegram.svg" alt="telegram icon" width={10} height={10} />
+              </a>
+            </li>
+            <li>
+              <a href="#" aria-label="X">
+                <Image src="/icons/x.svg" alt="X icon" width={10} height={10} />
+              </a>
+            </li>
+          </ul>
+        </div>
 
-export default Footer
+        <div className={styles.navBlock}>
+          <select className={styles.langSelect}>
+            <option value="en">English (EN)</option>
+            <option value="uk">Українська (UK)</option>
+          </select>
+          <div className={styles.links}>
+            <Link href="/terms">Terms & Conditions</Link>
+            <Link href="/privacy">Privacy Policy</Link>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.bottomSection}>
+        <p className={styles.text}>
+          KViTKA by Global Ukraine corporate accounts and cards are issued by GBH Canada (registration number OCN 1000765365, located at 80 Birmingham St, Unit C6, Toronto, Ontario, Canada, M8V 3W6), regulated by the Bank of Canada (MSB Registration: C100000033); and managed by GBH France (registration number 888 935 202, located at 8 Rue Graham Bell, 57000 Metz, France).
+        </p>
+        <p className={styles.copyright}>
+          © 2025 KVITKA Pro. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
