21d864b87890a9ea0682ba1f516ec367c0a5c44a
YuriiRogovets <yura.rogovets@gmail.com>
2025-07-16 13:27:43 +0300

    updated bg benefits

--- src/components/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits.jsx
@@ -4,30 +4,46 @@ import Image from "next/image";
 
 import styles from "./Benefits.module.css";
 
-
 const Benefits = () => {
   return (
-    <section className={styles.benefits}>
-      <div className={styles.container} >
-        <h2 className= {styles.title}>YOUR BENEFITS WITH US</h2>
-          <ul>
-            <li>
-              <span>Instant SEPA & SWIFT settlement</span>
-            </li>
-            <li>
-              <span>Crypto payments at market speed</span>
-            </li>
-            <li>
-              <span>Fast approval, global coverage</span>
-            </li>
-            <li>
-              <span>Community impact investments</span>
-            </li>
-          </ul>
-      </div>    
-      
-      </section>
-  )
-}
+    <div className={styles.benefits}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>YOUR BENEFITS WITH US</h2>
+        <ul>
+          <li>
+            <span>Instant SEPA & SWIFT settlement</span>
+          </li>
+          <li>
+            <span>Crypto payments at market speed</span>
+          </li>
+          <li>
+            <span>Fast approval, global coverage</span>
+          </li>
+          <li>
+            <span>Community impact investments</span>
+          </li>
+        </ul>
+      </div>
 
-export default Benefits
+      <Image
+        className={styles.LeftIcon}
+        src="/icons/kvitka-big-icon1.svg"
+        alt=" "
+        width={113}
+        height={85}
+
+      />
+
+      <Image
+        className={styles.RightIcon}
+        src="/icons/kvitka-small-icon.svg"
+        alt=" "
+        width={90}
+        height={90}
+
+      />
+    </div>
+  );
+};
+
+export default Benefits;
